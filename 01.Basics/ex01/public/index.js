f8c1f6a20a844cc0667178bc753be556804f79d9
BbingDdang <ddaa1541@gmail.com>
2024-07-10 10:27:43 +0900

    finish ex01

---
+++ 01.Basics/ex01/public/index.js
@@ -0,0 +1,9 @@
+function App() {
+    const App = document.createElement('div');
+    App.textContent = "Hello World!123";
+    
+
+    return App;
+}
+
+document.getElementById('root').appendChild(App());
