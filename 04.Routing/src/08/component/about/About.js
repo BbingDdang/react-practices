389863c45c561f13ff6406b074f92faf30c040f8
BbingDdang <ddaa1541@gmail.com>
2024-07-19 13:48:29 +0900

    04routing

--- 04.Routing/src/08/component/about/About.js
+++ 04.Routing/src/08/component/about/About.js
@@ -2,6 +2,11 @@ import React from 'react';
 import * as styles from '../../assets/scss/component/About.scss';
 
 export default function About() {
+    
+    // 서버에게 에러 요청
+    setTimeout(() => {
+        window.location.href = '/error'
+    }, 2000)
 
     return (
             <div className={styles.About}>
